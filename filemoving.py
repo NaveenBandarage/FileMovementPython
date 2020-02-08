c1302770c43ca4fd3f81ea2135294206b841ba95
amir tadros <amirtadros01@gmail.com>
2020-02-07 17:39:48 -0800

    Local File Organization
    Added support for organizing files within the local folder as a demo.

--- filemoving.py
+++ filemoving.py
@@ -3,23 +3,38 @@ from watchdog.observers import Observer
 
 #need to install these packages pip install watchdog
 
-import json 
+import json
 import os
 import time
 
 class MyHandler(FileSystemEventHandler):
-	def on_modified(self, event): #need to add an if statement to track what file extensions go where 
+	def on_created(self, event): #need to add an if statement to track what file extensions go where
 		for filename in os.listdir(folder_to_track):
-			src = folder_to_track + "/" + filename
-			newDestination = folderDestination + "/" + filename
-			os.rename(src, newDestination)
-folder_to_track = "/Users/naveenbandarage/Desktop/FileMovementPython/TestFolder"
-folderDestination = "/Users/naveenbandarage/Desktop/FileMovementPython/TestFolder2"
+			extension = os.path.splitext(filename)[-1].lower()
+			if extension in {'.png', '.jpg', '.jpeg'}:
+				src = folder_to_track + "/" + filename
+				newDestination = folderDestinatioPhotos + "/" + filename
+				os.rename(src, newDestination)
+			elif extension in {'.doc', '.xls', '.pdf', 'txt', '.docx'}:
+				src = folder_to_track + "/" + filename
+				newDestination = folderDestinationDocuments + "/" + filename
+				os.rename(src, newDestination)
+			elif extension in {'.mp3', '.wav'}:
+				src = folder_to_track + "/" + filename
+				newDestination = folderDestinationMusic + "/" + filename
+				os.rename(src, newDestination)
+
+folder_to_track = "/Users/amirt/GitHub/FileMovementPython/TestFolder"
+
+folderDestinationDocuments = "/Users/amirt/GitHub/FileMovementPython/TestFolderDocuments"
+folderDestinationMusic = "/Users/amirt/GitHub/FileMovementPython/TestFolderMusic"
+folderDestinationPhotos = "/Users/amirt/GitHub/FileMovementPython/TestFolderPhotos"
+
 eventHandler = MyHandler()
 observering = Observer()
 observering.schedule(eventHandler, folder_to_track, recursive=True)
 observering.start()
-try: 
+try:
 	while True:
 		time.sleep(10)
 except KeyboardInterrupt:
