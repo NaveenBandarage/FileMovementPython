249bb8539f9d7151528c34a31fc06782c7e2e6d9
amir tadros <amirtadros01@gmail.com>
2020-02-07 18:11:50 -0800

    Update filemoving.py

--- filemoving.py
+++ filemoving.py
@@ -8,15 +8,25 @@ import os
 import time
 import getpass
 
+def setup(user):
+	if not os.path.exists("/Users/" + user + "/Downloads/Photos"):
+		os.mkdir("/Users/" + user + "/Downloads/Photos")
+	if not os.path.exists("/Users/" + user + "/Downloads/Documents"):
+		os.mkdir("/Users/" + user + "/Downloads/Documents")
+	if not os.path.exists("/Users/" + user + "/Downloads/Music"):
+		os.mkdir("/Users/" + user + "/Downloads/Music")
+	if not os.path.exists("/Users/" + user + "/Downloads/Programs"):
+		os.mkdir("/Users/" + user + "/Downloads/Programs")
+
 class MyHandler(FileSystemEventHandler):
 	def on_created(self, event): #need to add an if statement to track what file extensions go where
 		for filename in os.listdir(folder_to_track):
 			extension = os.path.splitext(filename)[-1].lower()
-			if extension in {'.png', '.jpg', '.jpeg'}:
+			if extension in {'.png', '.jpg', '.jpeg', '.jfif'}:
 				src = folder_to_track + "/" + filename
 				newDestination = folder_destination_photos + "/" + filename
 				os.rename(src, newDestination)
-			elif extension in {'.doc', '.xls', '.pdf', 'txt', '.docx'}:
+			elif extension in {'.doc', '.xls', '.pdf', 'txt', '.docx', '.xlsx'}:
 				src = folder_to_track + "/" + filename
 				newDestination = folder_destination_documents + "/" + filename
 				os.rename(src, newDestination)
@@ -24,20 +34,28 @@ class MyHandler(FileSystemEventHandler):
 				src = folder_to_track + "/" + filename
 				newDestination = folder_destination_music + "/" + filename
 				os.rename(src, newDestination)
+			elif extension in {".exe", '.msi'}:
+				src = folder_to_track + "/" + filename
+				newDestination = folder_destination_programs + "/" + filename
+				os.rename(src, newDestination)
 
 
 user = getpass.getuser()
 
-folder_to_track = "/Users/" + user + "/GitHub/FileMovementPython/TestFolder"
+folder_to_track = "/Users/" + user + "/Downloads"
 
-folder_destination_documents = "/Users/" + user + "/GitHub/FileMovementPython/TestFolderDocuments"
-folder_destination_music = "/Users/" + user + "GitHub/FileMovementPython/TestFolderMusic"
-folder_destination_photos = "/Users/" + user + "/GitHub/FileMovementPython/TestFolderPhotos"
+folder_destination_documents = "/Users/" + user + "/Downloads/Documents"
+folder_destination_music = "/Users/" + user + "/Downloads/Music"
+folder_destination_photos = "/Users/" + user + "/Downloads/Photos"
+folder_destination_programs = "/Users/" + user + "/Downloads/Programs"
+
+setup(user)
 
 eventHandler = MyHandler()
 observering = Observer()
 observering.schedule(eventHandler, folder_to_track, recursive=True)
 observering.start()
+
 try:
 	while True:
 		time.sleep(10)
