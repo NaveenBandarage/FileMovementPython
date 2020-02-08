0922a9f16422435bf884c3de48308bea8fdd1777
amir tadros <amirtadros01@gmail.com>
2020-02-07 17:52:13 -0800

    retrives user
    Program now retrieves the user to access the right directory without hardcoding it in.

--- filemoving.py
+++ filemoving.py
@@ -6,6 +6,7 @@ from watchdog.observers import Observer
 import json
 import os
 import time
+import getpass
 
 class MyHandler(FileSystemEventHandler):
 	def on_created(self, event): #need to add an if statement to track what file extensions go where
@@ -13,22 +14,25 @@ class MyHandler(FileSystemEventHandler):
 			extension = os.path.splitext(filename)[-1].lower()
 			if extension in {'.png', '.jpg', '.jpeg'}:
 				src = folder_to_track + "/" + filename
-				newDestination = folderDestinatioPhotos + "/" + filename
+				newDestination = folder_destination_photos + "/" + filename
 				os.rename(src, newDestination)
 			elif extension in {'.doc', '.xls', '.pdf', 'txt', '.docx'}:
 				src = folder_to_track + "/" + filename
-				newDestination = folderDestinationDocuments + "/" + filename
+				newDestination = folder_destination_documents + "/" + filename
 				os.rename(src, newDestination)
 			elif extension in {'.mp3', '.wav'}:
 				src = folder_to_track + "/" + filename
-				newDestination = folderDestinationMusic + "/" + filename
+				newDestination = folder_destination_music + "/" + filename
 				os.rename(src, newDestination)
 
-folder_to_track = "/Users/amirt/GitHub/FileMovementPython/TestFolder"
 
-folderDestinationDocuments = "/Users/amirt/GitHub/FileMovementPython/TestFolderDocuments"
-folderDestinationMusic = "/Users/amirt/GitHub/FileMovementPython/TestFolderMusic"
-folderDestinationPhotos = "/Users/amirt/GitHub/FileMovementPython/TestFolderPhotos"
+user = getpass.getuser()
+
+folder_to_track = "/Users/" + user + "/GitHub/FileMovementPython/TestFolder"
+
+folder_destination_documents = "/Users/" + user + "/GitHub/FileMovementPython/TestFolderDocuments"
+folder_destination_music = "/Users/" + user + "GitHub/FileMovementPython/TestFolderMusic"
+folder_destination_photos = "/Users/" + user + "/GitHub/FileMovementPython/TestFolderPhotos"
 
 eventHandler = MyHandler()
 observering = Observer()
